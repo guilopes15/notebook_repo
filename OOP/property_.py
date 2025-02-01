4f57862803c6e502616cacb3ae7a8b2e8d8c6365
guilherme <guilherme-lopes58@hotmail.com>
2025-02-01 13:57:13 -0300

    Adicionando exemplos de property

--- OOP/property_.py
+++ OOP/property_.py
@@ -1,3 +1,4 @@
+#Exemplo 1
 class Pessoa():
     def __init__(self, nome, sobrenome):
         self.nome = nome
@@ -19,3 +20,34 @@ class Pessoa():
     def nome_completo(self):
         self._nome_completo = ''
 
+
+#Exemplo 2
+class ItemPedido:
+    def __init__(self, desc, pr_unit, qtd):
+        self.desc = desc
+        self.pr_unit = pr_unit
+        self.qtd = qtd
+
+
+    @property
+    def total(self):
+        return self.pr_unit * self.qtd
+    
+
+    @property
+    def qtd(self):
+        return self.__qtd
+    
+
+    @qtd.setter
+    def qtd(self, valor):
+        if valor < 1:
+            raise ValueError('Quantidade < 1')
+        
+        else:
+            self.__qtd = valor
+
+
+# Ao passar o agumento qtd na instancia de ItemPedido,
+# será executado o setter da property qtd, em vez do self.qtd do __init__.
+item = ItemPedido('Bike', 600, qtd=2)
