90bcf21bc2c80c06b62d2d0da2dcb67a2a7bd8f4
guilherme <guilherme-lopes58@hotmail.com>
2025-02-05 23:29:35 -0300

    Manipulando arquivos e diretorios com pathlib

---
+++ Built_ins/Pathlib_/desafio.py
@@ -0,0 +1,18 @@
+# Listar a pasta_0, que esta dentro da pasta files, casp existam arquivos.txt
+# dentro da pasta_0, crie uma nova pasta chamada desafio, 
+# e copie os arquivos para essa pasta.
+from pathlib import Path
+from shutil import rmtree
+
+
+output_path = Path('desafio_2')
+
+if output_path.exists():
+    rmtree(output_path, ignore_errors=True)
+
+output_path.mkdir()
+
+for file_ in Path('files/pasta_0').glob('*.txt'):
+    new_file = output_path / file_.name
+    new_file.write_text(file_.read_text())
+
---
+++ Built_ins/Pathlib_/file_.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+
+path_atual = Path() / 'Built_ins' / 'Pathlib_'
+pasta_0 = path_atual / 'files' / 'pasta_0'
+pasta_0.mkdir(parents=True, exist_ok=True) # Cria a pasta
+
+for item in pasta_0.iterdir(): # Percorre diretorios
+    print(item.name)
+
+arquivo_0 = pasta_0 / 'arquivo_0.txt'
+
+arquivo_0.exists() # False
+arquivo_0.is_file() # False
+arquivo_0.is_dir() # False
+
+arquivo_0.touch() # Cria a arquivo
+
+arquivo_0.exists() # True
+arquivo_0.is_file() # True
+
+arquivo_0.suffix # .txt
+arquivo_0.stem # nome do arquivo -> arquivo_0
+
+arquivo_1 = pasta_0 / 'arquivo.tar.gz'
+arquivo_1.suffixes # ['.tar', '.gz']
+
+arquivo_0.write_text('apenas um teste', encoding="utf-8")
+arquivo_0.read_text(encoding="utf-8")
+
+arquivo_renomeado = arquivo_0.rename('arquivo_renomeado')
+arquivo_renomeado.unlink() # remove o arquivo
+pasta_0.rmdir() # remove a pasta
