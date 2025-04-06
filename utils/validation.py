f76ee078d13968c1817e7c557a06fbcaac5610a5
guilherme <guilherme-lopes58@hotmail.com>
2025-04-06 11:03:03 -0300

    Adicionando opção de validação de fields com pydantic

--- utils/validation.py
+++ utils/validation.py
@@ -1,3 +1,31 @@
+from pydantic import BaseModel, field_validator
+import re
+
+
+class Usuario(BaseModel):
+    nome: str
+
+    @field_validator('nome')
+    @classmethod
+    def nome_sem_numeros_ou_caracteres(cls, v):
+        # Aceita letras com acento, cedilha e espaço
+        if not re.fullmatch(r'[A-Za-zÀ-ÿ\s]+', v):
+            raise ValueError('Nome deve conter apenas letras e espaços')
+        return v
+
+
+#class Usuario(BaseModel):
+    #nome: str
+
+    #@field_validator('nome')
+    #@classmethod
+    #def validar_nome(cls, valor: str) -> str:
+        #for char in valor:
+            #if not (char.isalpha() or char.isspace()):
+                #raise ValueError('O nome deve conter apenas letras e espaços')
+        #return valor
+
+
 def validation(data: dict) -> bool:
     return all([
         all([value for value in data.values()]),
@@ -5,5 +33,3 @@ def validation(data: dict) -> bool:
         data['cpf'].isnumeric(), # Obs: rever digitos numericos
         len(data['cpf']) == 11
     ])
-
-
